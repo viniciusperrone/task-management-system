9581989779cad3a88e17fddf07dc9331effedf41
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-07-22 01:51:19 -0300

    feat(iam): registe user in dashboard admin

--- backend/iam/admin.py
+++ backend/iam/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from iam.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    search_fields = ["username", "email", "nickname"]
