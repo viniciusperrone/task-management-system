e06705a6c3526b6f3e9797688f53681b66093dec
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-07-22 07:05:25 -0300

    feat(tickets): register Board and Column in admin

--- backend/tickets/admin.py
+++ backend/tickets/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from tickets.models import Ticket
+from tickets.models import Board, Column, Ticket
 
 
 @admin.register(Ticket)
@@ -46,3 +46,53 @@ class TicketAdmin(admin.ModelAdmin):
             },
         ),
     )
+
+class ColumnInline(admin.TabularInline):
+    model = Column
+    extra = 1
+    fields = ('name', 'position', 'color')
+    ordering = ('position',)
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'color', 'created_at', 'updated_at')
+    list_filter = ('owner', 'created_at')
+    search_fields = ('name', 'description', 'owner__username', 'owner__email')
+    autocomplete_fields = ('owner',)
+    readonly_fields = ('created_at', 'updated_at')
+    inlines = [ColumnInline]
+
+    fieldsets = (
+        (
+            "General Information",
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "color",
+                    "owner",
+                )
+            },
+        ),
+        (
+            "Metadata",
+            {
+                "classes": ("collapse",),
+                "fields": (
+                    "created_at",
+                    "updated_at",
+                ),
+            },
+        ),
+    )
+
+
+@admin.register(Column)
+class ColumnAdmin(admin.ModelAdmin):
+    list_display = ('name', 'board', 'position', 'color', 'updated_at')
+    list_filter = ('board', 'created_at')
+    search_fields = ('name', 'board__name')
+    autocomplete_fields = ('board',)
+    readonly_fields = ('created_at', 'updated_at')
+    ordering = ('board', 'position')
