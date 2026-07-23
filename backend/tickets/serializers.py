eb9fb50ec98cc07a9b32fe533403f9534d30614b
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-07-23 18:39:46 -0300

    feat: add TicketSerializer

--- backend/tickets/serializers.py
+++ backend/tickets/serializers.py
@@ -1,5 +1,30 @@
 from rest_framework import serializers
-from tickets.models import Column, Board
+from tickets.models import Column, Board, Ticket
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    formatted_number = serializers.CharField(read_only=True)
+    column_board_id = serializers.IntegerField(source="column.board.id", read_only=True, default=None)
+
+    class Meta:
+        model = Ticket
+        fields = [
+            'id',
+            'number',
+            'formatted_number',
+            'title',
+            'description',
+            'priority',
+            'due_date',
+            'column',
+            'column_board_id',
+            'position',
+            'owner',
+            'shared_users',
+            'created_at',
+            'updated_at'
+        ]
+        read_only_fields = ['id', 'number', 'formatted_number', 'position', 'owner', 'created_at', 'updated_at']
 
 
 class ColumnSerializer(serializers.ModelSerializer):
