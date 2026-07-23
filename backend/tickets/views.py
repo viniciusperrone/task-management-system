dae5139e7cc91bb1b017e6c70b6abad485963b32
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-07-23 20:38:27 -0300

    feat: add BoardViewSet

--- backend/tickets/views.py
+++ backend/tickets/views.py
@@ -1,13 +1,25 @@
+from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from tickets.models import Column
+from tickets.models import Column, Board
 from tickets.serializers import (
     ColumnSerializer,
+    BoardSerializer,
 )
 
 
+class BoardViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = BoardSerializer
 
-class ColumnViewSet():
+    def get_queryset(self):
+        return Board.objects.filter(owner=self.request.user).prefetch_related('columns')
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ColumnViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = ColumnSerializer
 
