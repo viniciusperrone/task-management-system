60770220f120e7913233dd521dcdd4e596e1d70e
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-07-23 18:33:04 -0300

    feat: add ColumnViwSet

--- backend/tickets/views.py
+++ backend/tickets/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from tickets.models import Column
+from tickets.serializers import (
+    ColumnSerializer,
+)
+
+
+class ColumnViewSet():
+    permission_classes = [IsAuthenticated]
+    serializer_class = ColumnSerializer
+
+    def get_queryset(self):
+        return Column.objects.filter(board__owner=self.request.user)
