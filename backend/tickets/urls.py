e8eda9ce9637e58ed1caa342685cba6c4c2c6fa4
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-07-24 18:38:46 -0300

    fix: define basename nos routers do DRF

--- backend/tickets/urls.py
+++ backend/tickets/urls.py
@@ -3,8 +3,8 @@ from tickets.views import BoardViewSet, ColumnViewSet, TicketViewSet
 
 
 router = DefaultRouter()
-router.register('board', BoardViewSet)
-router.register('column', ColumnViewSet)
-router.register('ticket', TicketViewSet)
+router.register('board', BoardViewSet, basename='board')
+router.register('column', ColumnViewSet, basename='column')
+router.register('ticket', TicketViewSet, basename='ticket')
 
 urlpatterns = router.urls
