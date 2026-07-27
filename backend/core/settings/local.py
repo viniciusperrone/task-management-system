844f6c802524493ae00d1452942bc827172aa4ca
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-07-27 03:36:56 -0300

    chore: install and configure django-cors-headers

--- backend/core/settings/local.py
+++ backend/core/settings/local.py
@@ -2,3 +2,4 @@ from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS = ['*']
+CORS_ALLOW_ALL_ORIGINS = True
--- backend/core/settings/prod.py
+++ backend/core/settings/prod.py
@@ -1,4 +1,11 @@
 from .base import *
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 DEBUG = False
 ALLOWED_HOSTS = ['*']
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = os.environ['CORS_ALLOWED_ORIGINS']
